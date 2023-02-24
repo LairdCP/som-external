16638d55523ebd0692eef2d6f85332e6e622afa5
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-02-24 16:00:54 -0500

    som-external: Fixed brcm_patchram and backports package overrides
    Buildroot allows BR2_EXTERNAL layers to expand override list for
    only certian virtual packages, brcm_patchram_plus and
    linux_backports are not virtual packages as such cannot be
    overriden, so convert lrd-brcm-patchram-plus and lrd-linux-backports
    into independent packages
    
    Bug 23036

--- board/backports/post_image.sh
+++ board/backports/post_image.sh
@@ -6,9 +6,9 @@ if [ "${BR2_LRD_DEVEL_BUILD}" == "y" ]; then
 		BR2_DL_DIR="$(sed -n 's,^BR2_DL_DIR="\(.*\)"$,\1,p' ${BR2_CONFIG})"
 
 	if [ -n "${BR2_DL_DIR}" ]; then
-		mkdir -p ${BR2_DL_DIR}/linux-backports
+		mkdir -p ${BR2_DL_DIR}/lrd-linux-backports
 		mkdir -p ${BR2_DL_DIR}/backports-test
-		cp ${BINARIES_DIR}/*.bz2 ${BR2_DL_DIR}/linux-backports
+		cp ${BINARIES_DIR}/*.bz2 ${BR2_DL_DIR}/lrd-linux-backports
 		cp ${BINARIES_DIR}/*.bz2 ${BR2_DL_DIR}/backports-test
 	fi
 fi
