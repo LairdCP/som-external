2fc8e17e48b081c0f54a9f0d6ed6fddadb598bc3
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-03-09 13:07:36 -0500

    som-external: Move Release branch ID into som-external
    Bug 23036

--- external.mk
+++ external.mk
@@ -1,2 +1,5 @@
+# Summit branch number, update for every branch
+export BR2_LRD_BRANCH := 11
+
 include $(sort $(wildcard $(BR2_EXTERNAL_LRD_SOM_PATH)/package/*/*.mk))
 include $(sort $(wildcard $(BR2_EXTERNAL_LRD_SOM_PATH)/toolchain/*/*.mk))
