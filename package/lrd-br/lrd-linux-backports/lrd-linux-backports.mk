db4003a4afdbc194c792200018029e3d6c55e477
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-03-20 12:39:12 -0400

    linux-backports: Fixed backports rebuild on every build
    Made linux dependency affect ordering but not time stamp
    
    Bug 23673

--- package/lrd-br/lrd-linux-backports/lrd-linux-backports.mk
+++ package/lrd-br/lrd-linux-backports/lrd-linux-backports.mk
@@ -120,7 +120,7 @@ $(eval $(kconfig-package))
 $(LRD_LINUX_BACKPORTS_DIR)/$(LRD_LINUX_BACKPORTS_KCONFIG_STAMP_DOTCONFIG): $(LRD_LINUX_BACKPORTS_DIR)/.stamp_check_kernel_version
 
 .SECONDEXPANSION:
-$(LRD_LINUX_BACKPORTS_DIR)/.stamp_check_kernel_version: $$(LINUX_DIR)/$$(LINUX_KCONFIG_STAMP_DOTCONFIG) linux
+$(LRD_LINUX_BACKPORTS_DIR)/.stamp_check_kernel_version: $$(LINUX_DIR)/$$(LINUX_KCONFIG_STAMP_DOTCONFIG) | linux
 	$(Q)KVER=$(LINUX_VERSION_PROBED); \
 	KVER_MAJOR=`echo $${KVER} | sed 's/^\([0-9]*\)\..*/\1/'`; \
 	KVER_MINOR=`echo $${KVER} | sed 's/^[0-9]*\.\([0-9]*\).*/\1/'`; \
