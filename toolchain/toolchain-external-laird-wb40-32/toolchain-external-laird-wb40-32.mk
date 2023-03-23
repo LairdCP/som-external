09bf29f16396439ece7846941f337cb59150ccb8
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-03-22 21:00:09 -0400

    som-external: Update build toolchans
    Bug 23673

---
+++ toolchain/toolchain-external-laird-wb40-32/toolchain-external-laird-wb40-32.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# toolchain-external-laird-wb40-32
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION = 10.0.0.202
+TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_SOURCE = wb40_32_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION).tar.gz
+
+ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
+TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_SITE = https://files.devops.rfpros.com/builds/linux/toolchain/$(TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION)
+else
+TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_SITE = https://github.com/LairdCP/wb-package-archive/releases/download/LRD-REL-$(TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION)
+endif
+
+$(eval $(toolchain-external-package))
