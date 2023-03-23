6751dd5262953e8d82f9f8d644b2cb9547484bb6
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-03-22 21:00:09 -0400

    som-external: Update build toolchans
    Bug 23673

---
+++ toolchain/toolchain-external-laird-bdimx6/toolchain-external-laird-bdimx6.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# toolchain-external-laird-bdimx6
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION = 11.0.0.17
+TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_SOURCE = bdimx6_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION).tar.gz
+
+ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
+TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_SITE = https://files.devops.rfpros.com/builds/linux/toolchain/$(TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION)
+else
+TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_SITE = https://github.com/LairdCP/wb-package-archive/releases/download/LRD-REL-$(TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION)
+endif
+
+$(eval $(toolchain-external-package))
