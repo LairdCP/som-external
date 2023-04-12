ba6a192ebb09136bb61a719f18f9c86c262b4bea
Steve.Braneky <stephen.braneky@lairdconnect.com>
2023-04-12 09:55:55 -0400

    Incorporate 45 & 50 CRDA DB
    Bug: 24261

--- board/wb50n/post_build.sh
+++ board/wb50n/post_build.sh
@@ -5,4 +5,7 @@ echo "WB50n POST BUILD legacy script: starting..."
 # source the common post build legacy script
 . ${BR2_EXTERNAL_LRD_SOM_PATH}/board/post_build_common_legacy.sh "${BUILD_TYPE}"
 
+[ ! -f ${TARGET_DIR}/lib/firmware/regulatory_50.db ] || \
+    ln -sfr ${TARGET_DIR}/lib/firmware/regulatory_50.db ${TARGET_DIR}/lib/firmware/regulatory.db
+
 echo "WB50n POST BUILD script: done."
