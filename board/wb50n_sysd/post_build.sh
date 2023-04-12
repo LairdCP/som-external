de4ac915ffe712e517b8a071243355b676da7d87
Steve.Braneky <stephen.braneky@lairdconnect.com>
2023-04-12 09:55:55 -0400

    Incorporate 45 & 50 CRDA DB
    Bug: 24261

--- board/wb50n_sysd/post_build.sh
+++ board/wb50n_sysd/post_build.sh
@@ -3,3 +3,6 @@ BUILD_TYPE="${2}"
 DEVEL_KEYS="${3}"
 
 . "${BOARD_DIR}/../post_build_common_60.sh" "${BOARD_DIR}"  "${BUILD_TYPE}"  "${DEVEL_KEYS}"
+
+[ ! -f ${TARGET_DIR}/lib/firmware/regulatory_50.db ] || \
+    ln -sfr ${TARGET_DIR}/lib/firmware/regulatory_50.db ${TARGET_DIR}/lib/firmware/regulatory.db
