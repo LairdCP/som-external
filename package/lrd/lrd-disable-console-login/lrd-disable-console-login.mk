f4124eceeb6e98c88ec4897841ca2e3be03e5c07
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-05-30 19:10:06 -0400

    som-external: lrd-disable-console-login: Do nothing for devel builds
    When executing devel builds BR2_LRD_DEVEL_BUILD or BR2_IG60_DEVEL
    do not disable console login. This is useful feature for debugging
    
    Bug 24212

--- package/lrd/lrd-disable-console-login/lrd-disable-console-login.mk
+++ package/lrd/lrd-disable-console-login/lrd-disable-console-login.mk
@@ -2,18 +2,23 @@
 # Disable Console Login
 ##########################################################################
 
-define LRD_DISABLE_CONSOLE_LOGIN_INSTALL_INIT_SYSTEMD
+ifeq ($(BR2_LRD_IG60_DEVEL)$(BR2_LRD_DEVEL_BUILD),)
+
+ifeq ($(BR2_INIT_SYSTEMD),y)
+define LRD_DISABLE_CONSOLE_LOGIN
 	$(INSTALL) -d $(TARGET_DIR)/etc/systemd/system-generators
 	ln -fs /dev/null $(TARGET_DIR)/etc/systemd/system-generators/systemd-getty-generator
 endef
 
-ifneq ($(BR2_INIT_SYSV)$(BR2_INIT_BUSYBOX),)
-define LRD_DISABLE_CONSOLE_LOGIN_SYSV
+LRD_DISABLE_CONSOLE_LOGIN_ROOTFS_PRE_CMD_HOOKS += LRD_DISABLE_CONSOLE_LOGIN
+else ifneq ($(BR2_INIT_SYSV)$(BR2_INIT_BUSYBOX),)
+define LRD_DISABLE_CONSOLE_LOGIN
 	$(SED) '/getty/d' "${TARGET_DIR}/etc/inittab"
 endef
 
-LRD_DISABLE_CONSOLE_LOGIN_TARGET_FINALIZE_HOOKS += LRD_DISABLE_CONSOLE_LOGIN_SYSV
+LRD_DISABLE_CONSOLE_LOGIN_ROOTFS_PRE_CMD_HOOKS += LRD_DISABLE_CONSOLE_LOGIN
 endif
 
 $(eval $(generic-package))
+endif
 
