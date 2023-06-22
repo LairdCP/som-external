5ad36448c1fa3cf735ac3776c965b44b626ef486
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-06-22 12:36:19 -0400

    som-extetrnal: Updated toolchains to gcc 12.3, 11.4
    Bug 23673

--- toolchain/toolchain-external-laird-wb40-32/toolchain-external-laird-wb40-32.mk
+++ toolchain/toolchain-external-laird-wb40-32/toolchain-external-laird-wb40-32.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION = 11.0.0.17
+TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION = 11.0.0.54
 TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_SOURCE = wb40_32_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_WB40_32_VERSION).tar.gz
 
 ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
