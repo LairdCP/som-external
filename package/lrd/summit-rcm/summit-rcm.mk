00975e96178781baf911cd9f346e4c66282d2b9d
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2023-07-20 14:25:56 -0400

    som-external: summit-rcm: Add support for v2 /bluetooth endpoints
    Bug 23993

--- package/lrd/summit-rcm/summit-rcm.mk
+++ package/lrd/summit-rcm/summit-rcm.mk
@@ -55,6 +55,9 @@ ifeq ($(BR2_PACKAGE_SUMMIT_RCM_V2_ROUTES),y)
 ifeq ($(BR2_PACKAGE_SUMMIT_RCM_ENABLE_SESSIONS),y)
 	SUMMIT_RCM_EXTRA_PACKAGES += summit_rcm/rest_api/v2/login
 endif
+ifeq ($(BR2_PACKAGE_SUMMIT_RCM_BLUETOOTH),y)
+	SUMMIT_RCM_EXTRA_PACKAGES += summit_rcm/rest_api/v2/bluetooth
+endif
 endif
 ifeq ($(BR2_PACKAGE_SUMMIT_RCM_LEGACY_ROUTES),y)
 	SUMMIT_RCM_EXTRA_PACKAGES += summit_rcm/rest_api/legacy
