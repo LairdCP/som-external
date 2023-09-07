5b361e16655333361adaf9fa92220e1e3babea4c
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2023-09-07 13:44:12 -0400

    som-external: Update python-uvicorn to 0.23.2
    Bug 25051
    PROD-546

--- package/lrd-br/python-uvicorn/python-uvicorn.mk
+++ package/lrd-br/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.22.0
+PYTHON_UVICORN_VERSION = 0.23.2
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/c6/dd/0d3bab50ab4ef8bec849f89fec2adc2fabcc397018c30e57d9f0d4009c5e
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/4c/b3/aa7eb8367959623eef0527f876e371f1ac5770a3b31d3d6db34337b795e6
 PYTHON_UVICORN_SETUP_TYPE = pep517
 PYTHON_UVICORN_LICENSE = BSD-3-Clause
 PYTHON_UVICORN_LICENSE_FILES = LICENSE.md
