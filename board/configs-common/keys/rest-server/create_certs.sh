8e32ec8f77819d2d7e8910408589356f5ff7f54b
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2023-10-11 22:10:51 -0400

    som-external: Generate new ECDSA-based default REST server certs/keys
    PROD-1469

--- board/configs-common/keys/rest-server/create_certs.sh
+++ board/configs-common/keys/rest-server/create_certs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Create self signed certificates.
 
-openssl req -out ca.csr -newkey rsa:2048 -nodes -keyout ca.key -config ca.cnf
+openssl req -out ca.csr -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -keyout ca.key -config ca.cnf
 openssl x509 -req -days 3650 -in ca.csr -signkey ca.key -out ca.crt
-openssl req -out server.csr -newkey rsa:2048 -nodes -keyout server.key -config server.cnf
+openssl req -out server.csr -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -keyout server.key -config server.cnf
 openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -in server.csr -out server.crt -sha256 -extfile san.ext
 
 rm ca.csr car.key server.csr -fr
