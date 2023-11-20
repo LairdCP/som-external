e4f2f6c61d0c5aaf0624d2ecd35a0f45d40e3ee7
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-11-20 17:45:11 -0500

    sdcsupp: summit-hostapd: Makefile cleanup
    Old workarounds for different issues no longer needed,
    so remove them
    
    Bug 23673

--- package/lrd/summit-hostapd/summit-hostapd.mk
+++ package/lrd/summit-hostapd/summit-hostapd.mk
@@ -27,10 +27,7 @@ SUMMIT_HOSTAPD_CONFIG = $(@D)/hostapd/config_openssl
 
 define SUMMIT_HOSTAPD_BUILD_CMDS
 	cp $(SUMMIT_HOSTAPD_CONFIG) $(@D)/hostapd/.config
-
-	CFLAGS="-I$(STAGING_DIR)/usr/include/libnl3 $(TARGET_CFLAGS) -Wall" \
-	LDFLAGS="$(TARGET_LDFLAGS)" PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
-		$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/hostapd CC="$(TARGET_CC)"
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/hostapd
 endef
 
 ifeq ($(BR2_PACKAGE_SUMMIT_HOSTAPD_HOSTAPD_CLI),y)
