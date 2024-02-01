061f4106f0df5d725335242c4aa5ff7b7858b900
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2024-02-01 17:00:41 -0500

    feat(Summit RCM): Preserve settings after WebLCM upgrade
    Refs: PROD-6153, PROD-6155

--- package/lrd/summit-rcm/factory_powerup_summit-rcm.sh
+++ package/lrd/summit-rcm/factory_powerup_summit-rcm.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+WEBLCM_USER_SETTINGS_FILE="/data/secret/weblcm-python/weblcm-settings.ini"
+SUMMIT_RCM_USER_SETTINGS_FILE="/data/secret/summit-rcm/summit-rcm-settings.ini"
+
 # If the summit-rcm secret directory does not exist (in the event of an upgrade from WebLCM), create
 # it and copy the default SSL certificates
 if [ ! -d /data/secret/summit-rcm ]; then
-    echo "Detected upgrade from WebLCM, restoring default configuration"
+    echo "Summit RCM configuration not detected, restoring settings"
     cp -r /usr/share/factory/etc/secret/summit-rcm /data/secret
+
+    # Copy the WebLCM user settings to Summit RCM
+    [ -f $WEBLCM_USER_SETTINGS_FILE ] && cp $WEBLCM_USER_SETTINGS_FILE $SUMMIT_RCM_USER_SETTINGS_FILE
 fi
