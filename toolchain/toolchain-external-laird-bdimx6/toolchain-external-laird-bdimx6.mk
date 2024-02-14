897052338b8d084b1552d2875eb6e4f416f9cca8
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2024-02-14 17:32:29 -0500

    Updated toolchain to 11.0.0.233
    PROD-5229

--- toolchain/toolchain-external-laird-bdimx6/toolchain-external-laird-bdimx6.mk
+++ toolchain/toolchain-external-laird-bdimx6/toolchain-external-laird-bdimx6.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION = 11.0.0.178
+TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION = 11.0.0.233
 TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_SOURCE = bdimx6_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_BDIMX6_VERSION).tar.gz
 
 ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
