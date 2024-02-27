93c5b7525e16ae15fabcf309f0616ea99caece7c
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2024-02-27 13:18:39 -0500

    feat(python-httptools): Bump version to 0.6.1
    Refs: PROD-6391

--- package/lrd-br/python-httptools/python-httptools.mk
+++ package/lrd-br/python-httptools/python-httptools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HTTPTOOLS_VERSION = 0.6.0
+PYTHON_HTTPTOOLS_VERSION = 0.6.1
 PYTHON_HTTPTOOLS_SOURCE = httptools-$(PYTHON_HTTPTOOLS_VERSION).tar.gz
-PYTHON_HTTPTOOLS_SITE = https://files.pythonhosted.org/packages/e3/3f/e976f6cf3da5e9dbda0096a0e65c1b109036a562658e020971ec54d007fb
+PYTHON_HTTPTOOLS_SITE = https://files.pythonhosted.org/packages/67/1d/d77686502fced061b3ead1c35a2d70f6b281b5f723c4eff7a2277c04e4a2
 PYTHON_HTTPTOOLS_SETUP_TYPE = setuptools
 PYTHON_HTTPTOOLS_LICENSE = MIT
 PYTHON_HTTPTOOLS_LICENSE_FILES = LICENSE
