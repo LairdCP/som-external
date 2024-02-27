db27ab398e7d2e47dfbceef86480132dd67d4511
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2024-02-27 11:17:31 -0500

    feat(python-awsiotsdk): Bump to version 1.21.0
    Refs: PROD-6391

--- package/lrd-br/python-awsiotsdk/python-awsiotsdk.mk
+++ package/lrd-br/python-awsiotsdk/python-awsiotsdk.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AWSIOTSDK_VERSION = 1.19.0
+PYTHON_AWSIOTSDK_VERSION = 1.21.0
 PYTHON_AWSIOTSDK_SOURCE = awsiotsdk-$(PYTHON_AWSIOTSDK_VERSION).tar.gz
-PYTHON_AWSIOTSDK_SITE = https://files.pythonhosted.org/packages/24/4e/ac6b70fd0044401c6ab7cc8b2e7cfd17406f908048e7ecf3df3c4840f41d
+PYTHON_AWSIOTSDK_SITE = https://files.pythonhosted.org/packages/1c/7d/c143fe073625f2ea6a9d79f4198856beb100f7386ba709fbac88f85ec86e
 PYTHON_AWSIOTSDK_SETUP_TYPE = setuptools
 PYTHON_AWSIOTSDK_LICENSE = Apache-2.0
 
