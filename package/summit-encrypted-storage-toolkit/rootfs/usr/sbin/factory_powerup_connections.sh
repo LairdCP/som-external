6fadeb9db9a3944f053a5b69db0ab675e08b4e84
John Nosky <john.nosky@ezurio.com>
2026-08-13 10:18:33 -0400

    encrypted-toolkit: drop obsolete per-boot system connection restore
    System connections now live in the read-only /usr/lib location, so the
    factory_powerup_connections.sh hook that restores them on every boot is
    no longer needed
    
    PROD-80399

--- package/summit-encrypted-storage-toolkit/rootfs/usr/sbin/factory_powerup_connections.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: LicenseRef-Ezurio-Clause
-# Copyright (C) 2024 Ezurio
-
-# Always copy over system connections, as the host connection is critical
-cp -r "${FACTORY_SETTING_SECRET_SOURCE}/NetworkManager/system-connections" "${USER_SETTINGS_SECRET_TARGET}/NetworkManager"
